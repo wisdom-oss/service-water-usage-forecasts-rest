14f7528743c1943454cde356804dbc4926d410ba
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-12-13 18:43:08 +0100

    Created basic settings class containing all needed settings for now

---
+++ models/__init__.py
@@ -0,0 +1,22 @@
+"""Model for sorting the data models used throughout this project"""
+from pydantic import BaseSettings, Field, stricturl
+
+
+class ServiceSettings(BaseSettings):
+    database_url: stricturl(tld_required=False, allowed_schemes={"mariadb+pymysql"}) = Field(
+        default=...,
+        env='DATABASE_DSN'
+    )
+    """URL pointing to the MariaDB/MySQL Database containing the water usage data"""
+
+    service_registry_url: str = Field(
+        default=...,
+        env='SERVICE_REGISTRY_HOST'
+    )
+    """URL Pointing to the Eureka Server installation of this instance"""
+
+    amqp_url: stricturl(tld_required=False, allowed_schemes={"amqp"}) = Field(
+        default=...,
+        env="AMQP_URL"
+    )
+    """URL containing the credentials and address of the message broker"""
