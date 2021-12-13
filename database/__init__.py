e340d9643188494a2c0eadefe4b8172f149ba327
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-12-13 18:47:03 +0100

    Added initial database orm model setup

--- database/__init__.py
+++ database/__init__.py
@@ -2,3 +2,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from models import ServiceSettings
+
+# Read the configuration of the service
+service_config = ServiceSettings()
+
+# Create a new database engine
+__db_engine = create_engine(
+    service_config.database_url, pool_recycle=90
+)
+
+# Create a Database Session used later on in the api
+DatabaseSession = sessionmaker(autocommit=False, autoflush=False, bind=__db_engine)
+
+# Create a declarative base for the orm table objects
+TableBase = declarative_base()
