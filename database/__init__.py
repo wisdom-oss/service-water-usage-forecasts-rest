36e72384129301681746cac836a6dea59f191ecc
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2021-12-13 18:42:36 +0100

    Created layout for database orm description

---
+++ database/__init__.py
@@ -0,0 +1,4 @@
+"""Module for organizing the database connections and operations"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
