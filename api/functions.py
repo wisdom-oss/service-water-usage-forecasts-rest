bd37aa4db4b8b0d61c8c2465d6a47f0a2c8567e5
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-04-11 12:53:58 +0200

    Changed the return type of the database update getter

--- api/functions.py
+++ api/functions.py
@@ -35,11 +35,11 @@ def get_last_database_update(
     query = (
         f"SELECT timestamp "
         f"FROM public.audit "
-        f"WHERE schema_name = {schema_name} "
+        f"WHERE schema_name = '{schema_name}' "
         f"ORDER BY timestamp DESC "
         f"LIMIT 1"
     )
     result = engine.execute(query)
     for time in result:
         logging.info(time)
-        return time
+        return datetime.datetime.fromtimestamp(time[0], tz=pytz.reference.LocalTimezone())
