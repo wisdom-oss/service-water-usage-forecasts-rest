4bbe95326baf2025d644c10b51a586e710c99aa3
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-04-11 12:54:24 +0200

    Created ORM Classes for the Geodata used for this service to handle spatial relations

--- database/tables.py
+++ database/tables.py
@@ -1,9 +1,12 @@
 """Object-Relational-Mapping classes for the used database tables"""
 import sqlalchemy.orm
+import geoalchemy2
 from . import engine
 
 metadata = sqlalchemy.MetaData(schema="water_usage")
-ORMDeclarationBase = sqlalchemy.orm.declarative_base(name="ORMDeclarationBase")
+geo_metadata = sqlalchemy.MetaData(schema="geodata")
+ORMDeclarationBase = sqlalchemy.orm.declarative_base(name="ORMDeclarationBase", metadata=metadata)
+GeodataDeclarationBase = sqlalchemy.orm.declarative_base(name='GeoDataORMDeclarationBase', metadata=geo_metadata)
 
 
 def initialize_mappings():
@@ -11,10 +14,10 @@ def initialize_mappings():
     Initialize the object-relational mapping classes for this service
     """
     ORMDeclarationBase.metadata.create_all(bind=engine())
+    GeodataDeclarationBase.metadata.create_all(bind=engine())
 
 
 class ConsumerGroup(ORMDeclarationBase):
-
     __tablename__ = "consumer_groups"
 
     id: int = sqlalchemy.Column(
@@ -27,6 +30,9 @@ class ConsumerGroup(ORMDeclarationBase):
 
     description: str = sqlalchemy.Column(sqlalchemy.Text)
     """The description of the consumer group"""
+
+    parameter: str = sqlalchemy.Column(sqlalchemy.Text, unique=True)
+    """The query parameter value by which this consumer group is identified"""
 
 
 class Usage(ORMDeclarationBase):
@@ -68,3 +74,71 @@ class Usage(ORMDeclarationBase):
 
     year: int = sqlalchemy.Column(sqlalchemy.Integer)
     """The year in which the usage has been recorded"""
+
+
+class Municipal(GeodataDeclarationBase):
+    """
+
+    """
+    __tablename__ = "nds_municipalities"
+
+    id: int = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name: str = sqlalchemy.Column(
+        sqlalchemy.String(length=254)
+    )
+
+
+class GeoMunicipal(GeodataDeclarationBase):
+    """A Municipal from the geodata storage of this project"""
+
+    __tablename__ = "nds_municipalities"
+
+    __table_args__ = {
+        'extend_existing': True
+    }
+
+    id: int = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name: str = sqlalchemy.Column(
+        sqlalchemy.String(length=254)
+    )
+
+    geom = sqlalchemy.Column(
+        geoalchemy2.Geometry('MULTIPOLYGON')
+    )
+
+
+class District(GeodataDeclarationBase):
+    """A district from the geodata storage of the project"""
+
+    __tablename__ = "nds_districts"
+
+    id: int = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name: str = sqlalchemy.Column(
+        sqlalchemy.String(length=254)
+    )
+
+    geom = sqlalchemy.Column(
+        geoalchemy2.Geometry('MULTIPOLYGON')
+    )
+
+    municipals = sqlalchemy.orm.relationship(
+        "GeoMunicipal",
+        primaryjoin="func.ST_Contains(foreign(District.geom), GeoMunicipal.geom).as_comparison(1,2)",
+        viewonly=True,
+        uselist=True
+    )
