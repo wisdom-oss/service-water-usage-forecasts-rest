76d991ad6949ce730cb6c931b8e5410825882a98
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-02-10 13:38:57 +0100

    chore: set up the initial sql queries

---
+++ res/queries.sql
@@ -0,0 +1,5 @@
+-- name: check-municipality-keys
+SELECT key FROM geodata.shapes WHERE key = ANY($1);
+
+-- name: check-consumer-groups
+SELECT parameter FROM water_usage.consumer_groups WHERE parameter = ANY($1);
