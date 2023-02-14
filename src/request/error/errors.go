f3d47380ea680703ca19405af0560159a3ecc318
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-02-14 10:50:42 +0100

    chore: adding some custom errors

--- src/request/error/errors.go
+++ src/request/error/errors.go
@@ -9,11 +9,17 @@ import (
 const MissingAuthorizationInformation = "MISSING_AUTHORIZATION_INFORMATION"
 const InsufficientScope = "INSUFFICIENT_SCOPE"
 const InternalError = "INTERNAL_ERROR"
+const UnsupportedForecastMethod = "UNSUPPORTED_FORECAST_METHOD"
+const InvalidShapeKey = "INVALID_SHAPE_KEY"
+const InvalidConsumerGroup = "INVALID_CONSUMER_GROUP"
 
 var titles = map[string]string{
 	MissingAuthorizationInformation: "Unauthorized",
 	InsufficientScope:               "Insufficient Scope",
 	InternalError:                   "Internal Error",
+	UnsupportedForecastMethod:       "Unsupported forecast method",
+	InvalidShapeKey:                 "Invalid Shape Key",
+	InvalidConsumerGroup:            "Invalid Consumer Group",
 }
 
 var descriptions = map[string]string{
@@ -21,11 +27,17 @@ var descriptions = map[string]string{
 		"however the request did not contain valid authorization information. Please check the request",
 	InsufficientScope: "The authorization was successful, " +
 		"but the resource is protected by a scope which was not included in the authorization information",
-	InternalError: "During the handling of the request an unexpected error occurred",
+	InternalError:             "During the handling of the request an unexpected error occurred",
+	UnsupportedForecastMethod: "The supplied forecast method is not supported by this module",
+	InvalidShapeKey:           "One of the shape keys you provided is not valid. Please check your request",
+	InvalidConsumerGroup:      "One of the consumer groups you provided is not valid. Please check your request",
 }
 
 var httpCodes = map[string]int{
 	MissingAuthorizationInformation: http.StatusUnauthorized,
 	InsufficientScope:               http.StatusForbidden,
 	InternalError:                   http.StatusInternalServerError,
+	UnsupportedForecastMethod:       http.StatusNotFound,
+	InvalidShapeKey:                 http.StatusUnprocessableEntity,
+	InvalidConsumerGroup:            http.StatusUnprocessableEntity,
 }
