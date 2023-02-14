d8faad24f9171946a7b4577c2e99ba2661e4fa1e
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-02-14 10:35:44 +0100

    chore: adding query to access all consumer groups

--- res/queries.sql
+++ res/queries.sql
@@ -3,3 +3,6 @@ SELECT key FROM geodata.shapes WHERE key = ANY($1);
 
 -- name: check-consumer-groups
 SELECT parameter FROM water_usage.consumer_groups WHERE parameter = ANY($1);
+
+-- name: get-consumer-groups
+SELECT DISTINCT parameter FROM water_usage.consumer_groups;
